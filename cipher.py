ddd56cbe342e2670cb115fc6375e64798fcdf939
cLeon08 <92564127+cLeon08@users.noreply.github.com>
2024-03-26 18:52:29 -0400

    Initial Commit

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
 # add your code here
+orgMsg = input("Please enter a sentence to encrpyt: ")
+newMsg = ""
+
+for i in range(len(orgMsg)):
+    char = orgMsg[i]
+    if (char.isupper()):
+        newMsg += chr((ord(char) + 5-65) % 26 + 65)
+    elif (char == " "):
+        newMsg += char
+    elif not (char.isalpha() or char.isdigit() or char == " " ):
+        newMsg += char
+    else:
+        newMsg += chr((ord(char) + 5-97) % 26 + 97)
+    
+print (newMsg)
